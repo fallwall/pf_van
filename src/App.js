c2a9c21114c3fb27c430edc1f2e6566e8a2a39f1
XX <fallwall19@gmail.com>
2020-04-27 18:13:31 -0700

    parallax customize size test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from './pages/Home';
 import Port from './pages/Port';
 import './App.css';
